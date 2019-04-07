f8903a1a99b9c798f335482dbcd148014995ceb7
SalvadorM <sronikle@gmail.com>
2019-04-07 13:14:35 -0400

    profile folder

---
+++ slide/src/profileComponent/ProfileScene.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+
+class ProfileScene extends Component {
+    render(){
+        return(
+            <div>
+                <h1>Render User Profile</h1>
+            </div>
+        )
+    }
+}
