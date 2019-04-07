fe97fbf545185988ea4dc0e9d013769194dba0fd
Daniel Segarra <danielsegarra36@gmail.com>
2019-04-07 14:33:03 -0400

    linting and formatting

--- slide/src/App.js
+++ slide/src/App.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route, Switch } from 'react-router-dom';
+import ProfileScene from './profileComponent/ProfileScene';
+// import logo from "./logo.svg";
 import './App.css';
 
-//react router 
-import {Route, Switch} from 'react-router-dom'
-//components
-import ProfileScene from './profileComponent/ProfileScene';
-
 class App extends Component {
-  // componentDidMount(){
-  //   fetch("https://us-central1-project-bc489.cloudfunctions.net/slide/lilchat/chats/21/messages/").then(function(res){return res.json()}).then(function(res){console.log(res)});
-  // }
+  componentDidMount() {
+    fetch('https://us-central1-project-bc489.cloudfunctions.net/slide/lilchat/chats/21/messages/')
+      .then(res => res.json())
+      .then(res => console.log(res));
+  }
+
   render() {
     return (
       <div className="App">
         <Switch>
-            <Route exact path='/profile' component={ProfileScene} />
+          <Route exact path="/profile" component={ProfileScene} />
         </Switch>
       </div>
     );
