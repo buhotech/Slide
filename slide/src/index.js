c772b990d818241d0506e1caa1c340d868829453
Daniel Segarra <danielsegarra36@gmail.com>
2019-04-07 22:40:47 -0400

    insects

--- slide/src/index.js
+++ slide/src/index.js
@@ -6,14 +6,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import './index.css';
 
-var config = {
-  apiKey: 'AIzaSyBqqqMDs221ywv0UZ8nHXWIe8WPViTfBzU',
-  authDomain: 'project-bc489.firebaseapp.com',
-  databaseURL: 'https://project-bc489.firebaseio.com',
-  projectId: 'project-bc489',
-  storageBucket: 'project-bc489.appspot.com',
-  messagingSenderId: '376964361270'
-};
+var config = {};
 firebase.initializeApp(config);
 
 ReactDOM.render(
