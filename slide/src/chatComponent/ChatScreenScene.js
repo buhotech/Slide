c7539be69f19f18979bbe65b5c516b6a1da6621c
Angel Checo <angel.r.checo@gmail.com>
2019-04-13 12:14:28 -0400

    Cont. Updated Chat Temp UI 4

--- slide/src/chatComponent/ChatScreenScene.js
+++ slide/src/chatComponent/ChatScreenScene.js
@@ -9,11 +9,13 @@ class ChatScreenScence extends Component {
     this.state = {
       messages: {},
       members: {},
-      characters_left: 1500
+      characters_left_res: 0,
+      characters_left: 0
     };
     this.sendMessage = this.sendMessage.bind(this);
     this.getChatInfo = this.getChatInfo.bind(this);
     this.getMembers = this.getMembers.bind(this);
+    this.characterDecreaseAnimation = this.characterDecreaseAnimation.bind(this);
   }
 
   sendMessage() {
@@ -83,6 +85,22 @@ class ChatScreenScence extends Component {
 
   componentDidUpdate() {
     this.scrollToBottom();
+    //this.characterDecreaseAnimation(this.state.characters_left_res, this.state.characters_left);
+  }
+
+  characterDecreaseAnimation(to, val) {
+    if (val == 0) return;
+
+    let speed = 50;
+
+    to = parseInt(to);
+    val = parseInt(val);
+    let t = this;
+    setTimeout(function() {
+      //
+      document.getElementById('characters_left').innerHTML = val;
+      if (val > to) t.characterDecreaseAnimation(to, val - 1);
+    }, speed);
   }
 
   componentDidMount() {
@@ -99,7 +117,20 @@ class ChatScreenScence extends Component {
       .database()
       .ref('lilchat/chats/21/chat_info/characters')
       .on('value', function(snap) {
+        c_screen.setState({ characters_left_res: snap.val() });
+        if (c_screen.state.characters_left_res != c_screen.state.characters_left)
+          c_screen.characterDecreaseAnimation(
+            c_screen.state.characters_left_res,
+            c_screen.state.characters_left
+          );
+      });
+
+    firebase
+      .database()
+      .ref('lilchat/chats/21/chat_info/characters')
+      .once('value', function(snap) {
         c_screen.setState({ characters_left: snap.val() });
+        console.log(c_screen.state.characters_left);
       });
     this.getMembers();
     //fetch("https://us-central1-project-bc489.cloudfunctions.net/slide/lilchat/chats/21/messages/").then(function(res){return res.json()}).then(function(res){c_screen.setState({"messages":res})});
@@ -109,7 +140,7 @@ class ChatScreenScence extends Component {
     let prev_name = '';
     let first_m = true;
     for (let message in this.state.messages) {
-      console.log(first_m, message);
+      //console.log(first_m, message);
       let temp = (
         <Message
           my_user="elpapi"
@@ -120,7 +151,7 @@ class ChatScreenScence extends Component {
           content={this.state.messages[message].content}
         />
       );
-      console.log(this.state.messages[message].username);
+      //console.log(this.state.messages[message].username);
       prev_name = this.state.messages[message].username;
       first_m = false;
 
@@ -131,7 +162,9 @@ class ChatScreenScence extends Component {
         <div className="navBar" />
         <div className="chatTopLabel">
           <div className="chatTopLabelText2wrap">
-            <p className="chatTopLabelText2">{this.state.characters_left}</p>
+            <p className="chatTopLabelText2" id="characters_left">
+              {this.state.characters_left}
+            </p>
           </div>
           <div className="chatTopLabelInfoWrap">
             <div className="chatTopLabelParticipant profile_pic_in_chat" />
