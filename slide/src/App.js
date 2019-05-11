6d737176eb4385dfb22628f5408f5a58ae290d2f
Daniel Segarra <danielsegarra36@gmail.com>
2019-05-10 21:58:32 -0400

    testing semantic

--- slide/src/App.js
+++ slide/src/App.js
@@ -3,12 +3,13 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 // import logo from './logo.svg';
 
-//components
+// components
 import ProfileScene from './profileComponent/ProfileScene';
 import LoginUserForm from './userComponent/form/LoginUser';
 import RegisterUserForm from './userComponent/form/RegisterUser';
 import ChatScreenScene from './chatComponent/ChatScreenScene';
 import Match from './components/match';
+import WordChoices from './lobby/wordChoices';
 import ChatScreenListScence from './currentChatsComponent/CurrentChatsListScene';
 import Navbar from './navbarComponent/Navbar';
 
@@ -24,10 +25,11 @@ class App extends Component {
         <Navbar />
         <Switch>
           {/* <Route exact path="/profile" component={ProfileScene} /> */}
-          <Route exact path="/match" component={Match} />
+          <Route exact path="/match-test" component={Match} />
+          <Route exact path="/word-grid-test" component={WordChoices} />
           <Route exact path="/login" component={LoginUserForm} />
           <Route exact path="/register" component={RegisterUserForm} />
-          <Route exact path="/chats" component={ChatScreenListScence} />
+          {/* <Route exact path="/chats" component={ChatScreenListScence} /> */}
           <Route exact path="/chats/:chat_id" component={ChatScreenScene} />
           <PrivateRoute exact path="/profile" component={ProfileScene} />
         </Switch>
---
+++ slide/src/lobby/wordChoices.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class WordChoices extends Component {
+  render() {
+    return (
+      <div class="ui grid">
+        <div class="four wide column" />
+        <div class="four wide column" />
+        <div class="four wide column" />
+        <div class="four wide column" />
+      </div>
+    );
+  }
+}
+
+export default WordChoices;
