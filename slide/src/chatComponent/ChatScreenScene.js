8777ce51cad4a0a69b88420f1451319c2899697c
Daniel Segarra <danielsegarra36@gmail.com>
2019-05-12 01:54:32 -0400

    commented out scss files that are not currently present

--- slide/src/chatComponent/ChatScreenScene.js
+++ slide/src/chatComponent/ChatScreenScene.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import * as firebase from 'firebase';
 import Message from './Message';
-import './styles/chatscreen.scss';
+// import './styles/chatscreen.scss';
 import words from '../wordsBankObjects';
 import LoadingView from '../Loading/LoadingView';
 
--- slide/src/chatComponent/Message.js
+++ slide/src/chatComponent/Message.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './styles/message.scss';
+// import './styles/message.scss';
 
 class Message extends Component {
   render() {
